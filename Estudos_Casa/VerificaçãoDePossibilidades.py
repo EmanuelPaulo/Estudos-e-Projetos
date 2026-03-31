fe6ed3315b7a6e578ae949ecd022c181103566e5
Paulo Emanuel <262132489+EmanuelPaulo@users.noreply.github.com>
2026-03-30 23:53:57 -0300

    Concertos no GIT

---
+++ Estudos_Casa/VerificaçãoDePossibilidades.py
@@ -0,0 +1,9 @@
+import gspread
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+import requests
+from datetime import datetime, timedelta
+import re
+import time
+
+
